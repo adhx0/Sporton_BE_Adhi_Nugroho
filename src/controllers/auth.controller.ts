2c7fa0899f1bd5af40177a7389c4fc3e7ab848df
adhi <adhinugroho676@gmail.com>
2026-02-08 15:40:55 +0700

    session-8

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,41 +1,45 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/user.model";
 
-const JWT_SECRET = process.env.JWT_SECRET || "@Sport0n";
+const JWT_SECRET = process.env.JWT_SECRET || "Sporton123";
 
 export const signin = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const {email, password} = req.body;
+  try {
+    const { email, password } = req.body;
 
-        const user = await User.findOne({email})
-        if (!user) {
-            res.status(400).json({message: "Invalid Credentials, Email not found"});
-            return;
-        }
-
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            res.status(400).json({message: "Invalid Credentials, Wrong Password"});
-            return;
-        }
-        const token = jwt.sign({id: user._id, email: user.email}, JWT_SECRET, {
-            expiresIn: "1d"
-        })
-
-        res.json({
-            token,
-            user: {
-                id: user._id,
-                name: user.name,
-                email: user.email,
-            }
-        })
-    } catch(error) {
-        console.error("Signin Error : ", error);
-        res.status(500).json({message: "Internal Server Error"});
+    // Check if user exists or not
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).json({ message: "Invalid Credentials, Email not found" });
+      return;
     }
+
+    // Validate Password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      res.status(400).json({ message: "Invalid Credentials, wrong password" });
+      return;
+    }
+
+    // Generate JWT (JSON Web Token)
+    const token = jwt.sign({ id: user._id, email: user.email }, JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error("Signin Error : ", error);
+    res.status(500).json({ message: "Internal Server  Error" });
+  }
 };
 
 export const initiateAdmin = async (
@@ -45,6 +49,7 @@ export const initiateAdmin = async (
   try {
     const { email, password, name } = req.body;
 
+    // Check if user data / entry is exist
     const count = await User.countDocuments({});
     if (count > 0) {
       res.status(400).json({
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,17 +1,18 @@
-import mongoose, {Schema, Document} from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
 export interface IUser extends Document {
-    email: string;
-    password: string;
-    name: string;
+  email: string;
+  password: string;
+  name: string;
 }
 
-const UserSchema: Schema = new Schema({
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    name: {type: String, required: true},
-},
-{ timestamps: true },
+const UserSchema: Schema = new Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+  },
+  { timestamps: true },
 );
 
 export default mongoose.model<IUser>("User", UserSchema);
